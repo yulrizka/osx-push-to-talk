22c0c1d106cd18511d19d1f2a7f181d7413f9250
Ahmy Yulrizka <yulrizka@gmail.com>
2015-03-17 18:38:58 +0100

    Initial Commit

---
+++ PushToTalk/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PushToTalk
+//
+//  Created by Ahmy Yulrizka on 17/03/15.
+//  Copyright (c) 2015 yulrizka. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
