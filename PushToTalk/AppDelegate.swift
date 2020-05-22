940784b08662d77c1cf1a400bee64a054cc0e92f
Jeremy Ellison <birdmanjeremy@gmail.com>
2020-05-22 04:58:06 -0700

    feat: Updated to support Catalina and multiple devices. (#14)
    - Working device select menu
    - Don't fail launch if we already have the permission.
    - Remove commented out code.
    - Add refresh devices menu item. Fix enable/disable so it's not overriden by a command keypress.
    - Refactor HotKey support into it's own class.

--- PushToTalk/AppDelegate.swift
+++ PushToTalk/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import Cocoa
 import AudioToolbox
+import AVKit
 
 @NSApplicationMain
 
@@ -16,30 +17,67 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var window: NSWindow!
     @IBOutlet weak var statusMenu: NSMenu!
     @IBOutlet weak var menuItemToggle: NSMenuItem!
+    @IBOutlet weak var deviceMenu: NSMenu!
+    @IBOutlet weak var hotkeyMenuItem: NSMenuItem!
     
-    let microphone = Microphone()
+    var microphone = Microphone()
+    var hotkey: HotKey?
     
     let statusItem = NSStatusBar.system.statusItem(withLength: -1)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+        self.hotkey = HotKey(microphone: microphone, menuItem: hotkeyMenuItem)
+        
+        switch AVCaptureDevice.authorizationStatus(for: .audio) {
+            case .notDetermined: // The user has not yet been asked for camera access.
+                AVCaptureDevice.requestAccess(for: .audio) { granted in
+                    if !granted {
+                        NSLog("Can't get access to the mic.")
+                        exit(1)
+                    }
+                }
+            
+            case .denied: // The user has previously denied access.
+                fallthrough
+            case .restricted: // The user can't grant access due to restrictions.
+                NSLog("Can't get access to the mic.")
+                exit(1)
+            default:
+                print("Already has permission");
+        }
+
         statusItem.menu = statusMenu
         self.microphone.statusUpdated = { (status) in
             self.menuItemToggle.title = status.title()
             self.statusItem.image = status.image()
         }
-        
         self.microphone.status = .Muted
+        self.refreshDevices(nil);
     }
     
     
     // MARK: Menu item Actions
     @IBAction func toggleAction(_ sender: NSMenuItem) {
-        self.microphone.toggle()
+        self.hotkey!.toggle()
     }
     
     @IBAction func menuItemQuitAction(_ sender: NSMenuItem) {
         self.microphone.status = .Speaking
         exit(0)
+    }
+    
+    @IBAction func refreshDevices(_ sender: NSMenuItem?) {
+        do {
+            try self.microphone.setupDeviceMenu(menu: deviceMenu)
+        } catch {
+            print("Unexpected Error: \(error).")
+            exit(1)
+        }
+    }
+    
+    @IBAction func recordNewHotKey(_ sender: NSMenuItem) {
+        self.hotkey!.recordNewHotKey()
     }
 }
 
---
+++ PushToTalk/HotKey.swift
@@ -0,0 +1,62 @@
+//
+//  HotKey.swift
+//  PushToTalk
+//
+//  Created by Jeremy Ellison on 5/14/20.
+//  Copyright © 2020 yulrizka. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class HotKey {
+    
+    var enabled = true
+    let microphone: Microphone
+    let menuItem: NSMenuItem
+    var keyCode: UInt16 = 61
+    var modifierFlags = NSEvent.ModifierFlags.option
+    var recordingHotKey = false;
+    
+    init(microphone: Microphone, menuItem: NSMenuItem) {
+        self.menuItem = menuItem
+        self.menuItem.title = "Change HotKey (\(keyCode))"
+        
+        self.microphone = microphone
+        // handle when application is on background
+        NSEvent.addGlobalMonitorForEvents(matching: NSEvent.EventTypeMask.flagsChanged, handler: self.handleFlagChangedEvent)
+        
+        // handle when application is on foreground
+        NSEvent.addLocalMonitorForEvents(matching: NSEvent.EventTypeMask.flagsChanged, handler: { (theEvent) -> NSEvent? in
+            self.handleFlagChangedEvent(theEvent)
+            return theEvent
+        })
+    }
+    
+    func toggle() {
+        if (self.enabled == true) {
+            microphone.status = MicrophoneStatus.Speaking
+            self.enabled = false
+        } else {
+            microphone.status = MicrophoneStatus.Muted
+            self.enabled = true
+        }
+    }
+    
+    func recordNewHotKey() {
+        recordingHotKey = true;
+    }
+    
+    internal func handleFlagChangedEvent(_ theEvent: NSEvent!) {
+        if self.recordingHotKey {
+            self.recordingHotKey = false
+            self.keyCode = theEvent.keyCode
+            self.modifierFlags = theEvent.modifierFlags
+            self.menuItem.title = "Change HotKey (\(keyCode))"
+            return;
+        }
+        guard theEvent.keyCode == self.keyCode else { return }
+        guard self.enabled else { return }
+        microphone.status = (theEvent.modifierFlags.contains(self.modifierFlags)) ? .Speaking : .Muted
+    }
+}
