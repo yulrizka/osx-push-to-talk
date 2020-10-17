0763d6a6e74c650900bc8d4226cca13249b6dcfc
Ahmy Yulrizka <yulrizka@users.noreply.github.com>
2020-10-17 19:39:17 +0200

    feat: persist hotkey

--- PushToTalk/HotKey.swift
+++ PushToTalk/HotKey.swift
@@ -12,29 +12,51 @@ import AppKit
 class HotKey {
     var doubletap = false
     var previousEpoc = NSDate().timeIntervalSince1970
-    
+
+    let prefKeyCode = "prefKeyCode"
+    let prefModifiers = "prefModifier"
+
     var enabled = true
     let microphone: Microphone
     let menuItem: NSMenuItem
-    var keyCode: UInt16 = 61
-    var modifierFlags = NSEvent.ModifierFlags.option
+
+    var keyCode: UInt16
+    var modifierFlags:NSEvent.ModifierFlags
+    let defaultKeyCode: UInt16 = 61
+    let defaultModifierFlags = NSEvent.ModifierFlags.option
+
     var recordingHotKey = false;
-    
+
     init(microphone: Microphone, menuItem: NSMenuItem) {
+        self.keyCode = defaultKeyCode
+        self.modifierFlags = defaultModifierFlags
+
+        // load key from preference
+        self.keyCode = UInt16(UserDefaults.standard.integer(forKey: prefKeyCode))
+        if self.keyCode == 0 {
+            self.keyCode = defaultKeyCode
+        }
+
+        if UserDefaults.standard.object(forKey: prefModifiers) != nil {
+            let modifierFlags:UInt = UInt(UserDefaults.standard.integer(forKey: prefModifiers))
+            self.modifierFlags = NSEvent.ModifierFlags(rawValue: modifierFlags)
+        }
+        print("loaded keycode \(self.keyCode) \(self.modifierFlags)")
+
         self.menuItem = menuItem
         self.menuItem.title = "Change HotKey (\(keyCode))"
-        
+
         self.microphone = microphone
         // handle when application is on background
         NSEvent.addGlobalMonitorForEvents(matching: NSEvent.EventTypeMask.flagsChanged, handler: self.handleFlagChangedEvent)
-        
+
         // handle when application is on foreground
         NSEvent.addLocalMonitorForEvents(matching: NSEvent.EventTypeMask.flagsChanged, handler: { (theEvent) -> NSEvent? in
             self.handleFlagChangedEvent(theEvent)
             return theEvent
         })
     }
-    
+
     func toggle() {
         if (self.enabled == true) {
             microphone.status = MicrophoneStatus.Speaking
@@ -44,29 +66,31 @@ class HotKey {
             self.enabled = true
         }
     }
-    
+
     func recordNewHotKey() {
         recordingHotKey = true;
     }
-    
+
     func checkForDoubleTap() {
         let timeInterval = NSDate().timeIntervalSince1970
         let timediff = timeInterval - self.previousEpoc
         self.previousEpoc = timeInterval
         self.doubletap = timediff < 0.2
     }
-    
+
     internal func handleFlagChangedEvent(_ theEvent: NSEvent!) {
         if self.recordingHotKey {
             self.recordingHotKey = false
             self.keyCode = theEvent.keyCode
             self.modifierFlags = theEvent.modifierFlags
-            self.menuItem.title = "Change HotKey (\(keyCode))"
+            self.menuItem.title = "Change HotKey (\(keyCode) (\(modifierFlags)"
+            UserDefaults.standard.set(self.keyCode, forKey: prefKeyCode)
+            UserDefaults.standard.set(self.modifierFlags.rawValue, forKey: prefModifiers)
             return;
         }
         guard theEvent.keyCode == self.keyCode else { return }
         guard self.enabled else { return }
-        
+
         if theEvent.modifierFlags.contains(self.modifierFlags) {
             checkForDoubleTap()
             microphone.status = .Speaking
